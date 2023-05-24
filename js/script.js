2e2650821a89cd21e9b87be559f8f75c123278b5
Patrick Coxall <mr.coxall@mths.ca>
2023-05-24 08:30:48 -0400

    PWA test

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-charlotte-jhu/sw.js", {
+    scope: "/ICS2O-Unit-1-08-charlotte-jhu/",
+  })
+}
+
 function myButtonClicked() {
   alert("You clicked the button!");
 }
